da236058d768681137d6ab8ad22c28f0b8cf67d8
anj0la <anjola.aina@gmail.com>
2026-04-24 12:12:52 -0600

    Add read_line_comment Created first recursive function in OCaml! Converted old C++ code into idomatic OCaml version with no while / for loop.

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -8,8 +8,11 @@ type lexer = {
     loc: source_location;
 }
 
+let is_at_end lex =
+    lex.curr >= String.length lex.source
+
 let peek lex = 
-    if lex.curr >= String.length lex.source then
+    if is_at_end lex then
         None
     else
         Some lex.source.[lex.curr] (* returns the token at the current pointer *)
@@ -23,8 +26,23 @@ let make_token lex kind =
         String.sub lex.source lex.start (lex.curr - lex.start) in
     { kind = kind; lexeme = text; loc = lex.loc }
 
+let rec read_line_comment lex = 
+    if peek lex == Some '\n' || is_at_end lex then ()
+    else
+        let (_, lex') = advance lex in
+        read_line_comment lex'
+
 let is_digit c = 
     c >= '0' && c <= '9'
+
+let is_hex_digit c =
+    is_digit c || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F')
+
+let is_binary_digit c =
+    c == '0' || c == '1'
+
+let is_octal_digit c =
+	c >= '0' && c <= '7'
 
 let is_alpha c =
     (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
@@ -36,6 +54,7 @@ let is_symbol c =
     (c >= '!' && c <= '/') || (c >= ':' && c <= '@') || (c >= '[' && c <= '`') || (c >= '{' && c <= '~')
 
 let is_space c =
-    c == '_'
-
+    c == ' '
 	
+let is_printable c =
+    is_alpha c || is_digit c || is_symbol c || is_space c
