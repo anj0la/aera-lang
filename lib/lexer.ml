ba36469d2f8fd74e3df0c04ab1c17f73080e59c2
anj0la <anjola.aina@gmail.com>
2026-04-24 05:35:55 -0600

    Add more lexer functions Added the peek, advance and make_token (equiv to add_token). Created boolean functions to check for digit, alpha, etc. Converted them from C++ to OCaml. Handwritten.

--- lib/lexer.ml
+++ lib/lexer.ml
@@ -1,1 +1,41 @@
-let () = print_endline "Hello world!";
+open Source_location
+open Token
+
+type lexer = {
+    source: string;
+    start: int;
+    curr: int;
+    loc: source_location;
+}
+
+let peek lex = 
+    if lex.curr >= String.length lex.source then
+        None
+    else
+        Some lex.source.[lex.curr] (* returns the token at the current pointer *)
+
+let advance lex =
+    let c = lex.source.[lex.curr] in 
+    (c, { lex with curr = lex.curr + 1 }) (* creates a NEW lexer record with the updated current pointer*)
+
+let make_token lex kind =
+    let text = 
+        String.sub lex.source lex.start (lex.curr - lex.start) in
+    { kind = kind; lexeme = text; loc = lex.loc }
+
+let is_digit c = 
+    c >= '0' && c <= '9'
+
+let is_alpha c =
+    (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+
+let is_alnum c =
+    is_alpha c || is_digit c || c == '_'
+
+let is_symbol c =
+    (c >= '!' && c <= '/') || (c >= ':' && c <= '@') || (c >= '[' && c <= '`') || (c >= '{' && c <= '~')
+
+let is_space c =
+    c == '_'
+
+	
